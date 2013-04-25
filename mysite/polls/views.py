1c96296b89dbfe45bb93c989337bacab63bfad7a
ysyfff <1156546473@qq.com>
2013-04-25 15:58:29 +0800

    重新认识template的布局和{%url%}的引用

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,64 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the poll index.")
+
+
+
+# from django.http import HttpResponse
+
+# from polls.models import Poll
+
+# def index(request):
+#     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([p.question for p in latest_poll_list])
+#     return HttpResponse(output)
+
+
+
+# from django.http import HttpResponse
+# from django.template import Context, loader
+
+# from polls.models import Poll
+
+# def index(request):
+#     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = Context({
+#         'latest_poll_list': latest_poll_list,
+#     })
+#     return HttpResponse(template.render(context))
+
+
+
+from django.shortcuts import render
+
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
+
+# def detail(request, poll_id):
+#     return HttpResponse("You're looking at poll %s." % poll_id)
+
+# from django.http import Http404
+# # ...
+# def detail(request, poll_id):
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#     return render(request, 'polls/detail.html', {'poll': poll})
+
+
+from django.shortcuts import render, get_object_or_404
+# ...
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
