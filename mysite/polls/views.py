73d9354b8092df675da682c8f54e0542a9dc1018
ysyfff <1156546473@qq.com>
2013-04-25 18:30:57 +0800

    使用了新的generic views:less code is better

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -32,13 +32,14 @@
 
 
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 from polls.models import Poll
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_poll_list': latest_poll_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#     context = {'latest_poll_list': latest_poll_list}
+#     return render(request, 'polls/index.html', context)
 
 
 # def detail(request, poll_id):
@@ -55,14 +56,14 @@ def index(request):
 
 
 from django.shortcuts import render, get_object_or_404
-# ...
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+# # ...
+# def detail(request, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
+#     return render(request, 'polls/detail.html', {'poll': poll})
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+# def results(request, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
+#     return render(request, 'polls/results.html', {'poll': poll})
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
